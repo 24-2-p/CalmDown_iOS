bd45b2510b64b49a13c0b116d38b46e8325bff86
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-09-20 16:02:25 +0900

    🙈 :: Updated .gitignore

---
+++ CalmDown/CalmDown/CalmDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalmDownApp.swift
+//  CalmDown
+//
+//  Created by 채리원 on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CalmDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
