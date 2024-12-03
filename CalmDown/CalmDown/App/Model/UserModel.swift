9e23bf8071e8f23bd288e28f885e60e71f5899cb
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-12-03 13:55:52 +0900

    ✨ :: UserModel

---
+++ CalmDown/CalmDown/App/Model/UserModel.swift
@@ -0,0 +1,15 @@
+//
+//  UserModel.swift
+//  CalmDown
+//
+//  Created by 채리원 on 12/3/24.
+//
+
+import Foundation
+
+struct UserModel {
+    var username: String = ""
+    var email: String = ""
+    var password: String = ""
+    var confirmPassword: String = ""
+}
