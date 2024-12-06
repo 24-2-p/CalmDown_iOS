d445b63a56a96964947a403fbeaad32cd3ed04f5
sangyup <ideallyup@naver.com>
2024-12-07 01:14:20 +0900

    :sparkles: :: Add Tabbar

--- CalmDown/CalmDown/App/View/ContentView.swift
+++ CalmDown/CalmDown/App/View/ContentView.swift
@@ -5,11 +5,31 @@
 //  Created by 채리원 on 9/20/24.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        StartView()
+        TabView {
+            MatchingView()
+                .tabItem {
+                    Image("homeTabbar")
+                    Text("홈")
+                }
+            
+            MatchingView()
+                .tabItem {
+                    Image("mainTabbar")
+                    Text("매칭")
+                }
+            
+            TechSelectionView()
+                .tabItem {
+                    Image("profileTabbar")
+                    Text("내 정보")
+                }
+        }
+        .accentColor(Color.deepBlue)
     }
 }
 
