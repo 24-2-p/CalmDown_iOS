b5704b8d160c6f80b21ddc962db2a031e0e6a4fa
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-12-06 15:01:58 +0900

    🍱 :: Add profile Image

---
+++ CalmDown/CalmDown/App/View/HomeMatchingView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeMatchingView.swift
+//  CalmDown
+//
+//  Created by 채리원 on 12/6/24.
+//
+
+import SwiftUI
+
+struct HomeMatchingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeMatchingView()
+}
