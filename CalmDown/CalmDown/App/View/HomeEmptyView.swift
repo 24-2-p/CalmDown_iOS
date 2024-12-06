4f02c66d4ec5d69081ab0eb99a584a4becb4dd39
riwon <137711369+cherry-p0p@users.noreply.github.com>
2024-12-06 14:54:30 +0900

    ✨ :: HomeEmptyView

---
+++ CalmDown/CalmDown/App/View/HomeEmptyView.swift
@@ -0,0 +1,38 @@
+//
+//  HomeEmptyView.swift
+//  CalmDown
+//
+//  Created by 채리원 on 12/6/24.
+//
+
+import SwiftUI
+
+struct HomeEmptyView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Text("Calm Match")
+                    .font(.pretendard(.medium, size: 24))
+                    .foregroundColor(Color.deepBlue)
+                Spacer()
+                Image("MainLogo")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+            }
+            .padding(.horizontal, 33)
+            .padding(.bottom, 119)
+            
+            Image("person.3")
+                .padding(.bottom, 17)
+            
+            Text("매칭된 팀이 없습니다.")
+                .font(.pretendard(.medium, size: 20))
+                .foregroundColor(Color.MediumGray)
+        }
+        Spacer()
+    }
+}
+
+#Preview {
+    HomeEmptyView()
+}
